d2ea8630783c86ee83811340681985f4345728c4
Nagashree30 <74227750+Nagashree30@users.noreply.github.com>
2020-12-25 22:16:15 +0530

    Add files via upload

---
+++ IntToFloat.java
@@ -0,0 +1,32 @@
+package com.xworkz.xworkzapp.tcasting;
+
+public class IntToFloat {
+	public static void main(String[] args) {
+		int myInt=20;
+		float myFloat = myInt; // Automatic casting: int to float
+
+	    System.out.println(myInt);      
+	    System.out.println(myFloat);  
+	    
+	    int myInt1 = 9;
+	    double myDouble = myInt1; // Automatic casting: int to double
+
+	    System.out.println(myInt1);      
+	    System.out.println(myDouble); 
+	    
+	    int myInt3 = 990079477;
+	    long myLong = myInt3; // Automatic casting: int to long
+
+	    System.out.println(myInt3);      
+	    System.out.println(myLong); 
+		
+	    byte myByte=124;
+	    short myShort=myByte;
+	    System.out.println(myByte);      
+	    System.out.println(myShort); 
+	    
+	    
+	    
+	}
+
+}
---
+++ TypeCasting.java
@@ -0,0 +1,25 @@
+package com.xworkz.xorkzapp.typecasting;
+
+public class TypeCasting {
+
+		  public static void main(String[] args) {
+			  double myDouble = 9.78;
+			    int myInt = (int) myDouble; // Manual casting: double to int
+
+			    System.out.println(myDouble);   
+			    System.out.println(myInt);     
+			    
+			    float myFloat = 12.89f;
+			    int myLong = (int) myFloat; // Manual casting: float to long
+			    System.out.println(myFloat);   
+			    System.out.println(myLong); 
+			    
+			    double myLong1 = 9.78;
+			    int myInt1 = (int) myLong1; // Manual casting:long to int
+
+			    System.out.println(myLong1);   
+			    System.out.println(myInt1); 
+			  }   
+		  }
+		
+
